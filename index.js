31a8c7b218241415883ad0c52e0d3c9737f0b5ab
usavich <102706849+usavich1963@users.noreply.github.com>
2022-05-12 12:14:54 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,15 +4,19 @@ var key = params.get('key');
 $(function () {
     // 送信
     $('form').submit(function () {
-        var reasons = $('textarea[name="comname"]').val();
-        var reasons = $('textarea[name="telnum"]').val();
-        var reasons = $('textarea[name="hplink"]').val();
-        var reasons = $('textarea[name="cont"]').val();
+        var comname = $('textarea[name="comname"]').val();
+        var telnum = $('textarea[name="telnum"]').val();
+        var hplink = $('textarea[name="hplink"]').val();
+        var cont = $('textarea[name="cont"]').val();
         // var date = $('input[name="date"]').val();
         // var tool = $('input[name="tool"]').val();
         
         //var msg = `${key}\n朝礼/終礼不参加の理由：${reasons}\n返信期限：${date}\n使用ツール：${tool}`;
-        var msg = `${key}\n理由：${reasons}`;
+        var msg = `${key}\n理由：${comname}`;
+        var msg = `${key}\n理由：${telnum}`;
+        var msg = `${key}\n理由：${hplink}`;
+        var msg = `${key}\n理由：${cont}`;
+               
         sendText(msg);
 
         return false;
