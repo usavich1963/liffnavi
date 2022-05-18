015667ede41a8f4656db4d77dff7c7bcb183f010
usavich <102706849+usavich1963@users.noreply.github.com>
2022-05-18 09:00:14 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ $(function () {
     // 送信
     $('form').submit(function () {
         var reasons = $('textarea[name="reasons"]').val();
-        var date = $('input[name="date"]').val();
+        var name = $('input[name="name"]').val();
+        var furigana = $('input[name="furigana"]').val();
+        var name = $('input[name="name"]').val();
+        var name = $('input[name="name"]').val();
         // var tool = $('input[name="tool"]').val();
         
         //var msg = `${key}\n朝礼/終礼不参加の理由：${reasons}\n返信期限：${date}\n使用ツール：${tool}`;
