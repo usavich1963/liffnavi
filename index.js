4e4b6ba967aa74d57b04178f209752dc26ab20af
usavich <102706849+usavich1963@users.noreply.github.com>
2022-05-18 09:20:39 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,15 +4,16 @@ var key = params.get('key');
 $(function () {
     // 送信
     $('form').submit(function () {
-        var reasons = $('textarea[name="reasons"]').val();
+        
         var name = $('input[name="name"]').val();
         var furigana = $('input[name="furigana"]').val();
         var email = $('input[name="email"]').val();
         var tel = $('input[name="tel"]').val();
+        var reasons = $('textarea[name="reasons"]').val();
         // var tool = $('input[name="tool"]').val();
         
         //var msg = `${key}\n朝礼/終礼不参加の理由：${reasons}\n返信期限：${date}\n使用ツール：${tool}`;
-        var msg = ``求人内容：\n${name}\n${furigana}\n${email}\n${tel}`;
+        var msg = `求人内容：\n${name}\n${furigana}\n${email}\n${tel}\n${reasons}`;
                
         sendText(msg);
 
